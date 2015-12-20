196ac538985f5d8d1c0880732cc6f6c23f2beb09
Omid Nasri <omid.nasri@outlook.com>
2015-12-20 21:24:07 +0330

    12-20-2015

---
+++ PostMessageToFacebook/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PostMessageToFacebook
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // پارامتر اول کد دریافتی از فیس بوک در گام سوم دریافت نمودید درج خواهد شد
+            Console.WriteLine(PostFacebookWall("Access Token", "Your Message"));
+        }
+        private static string PostFacebookWall(string accessToken, string message)
+        {
+            var responsePost = "";
+            try
+            {
+                //create the facebook account object
+                var objFacebookClient = new Facebook.FacebookClient(accessToken);
+                var parameters = new Dictionary<string, object>();
+                parameters["message"] = message;
+                responsePost = objFacebookClient.Post("feed", parameters).ToString();
+            }
+            catch (Exception ex)
+            {
+                responsePost = "Facebook Posting Error Message: " + ex.Message;
+            }
+            return responsePost;
+        }
+    }
+}
